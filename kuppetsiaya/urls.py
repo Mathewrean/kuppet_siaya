a043ddab5b419bd7b722be6665734d13dcc17756
Benard Karanja <benardopeter4@gmail.com>
2026-04-24 08:26:47 +0300

    Minor changes

--- kuppetsiaya/urls.py
+++ kuppetsiaya/urls.py
@@ -18,10 +18,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
     path('accounts/', include('accounts.urls')),
     path('dashboard/', include('dashboard.urls')),
+    path('favicon.ico', serve, {'document_root': settings.STATICFILES_DIRS[0], 'path': 'favicon.ico'}),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
