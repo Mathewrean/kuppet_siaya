8eec4539b258f9ca225ce56c8144b4a3af5b48d2
Benard Karanja <benardopeter4@gmail.com>
2026-04-25 21:09:44 +0300

    Production version

--- kuppetsiaya/settings.py
+++ kuppetsiaya/settings.py
@@ -107,7 +107,7 @@ ROOT_URLCONF = 'kuppetsiaya.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
