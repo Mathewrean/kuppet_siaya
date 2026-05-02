718006d0e23f39adacc1bc5276ed3e77243b9d81
Benard Karanja <benardopeter4@gmail.com>
2026-05-02 11:22:20 +0300

    Minor changes

--- core/apps.py
+++ core/apps.py
@@ -3,3 +3,10 @@ from django.apps import AppConfig
 
 class CoreConfig(AppConfig):
     name = 'core'
+
+    def ready(self):
+        from kuppetsiaya.admin_security import apply_admin_security
+        from kuppetsiaya.compat import apply_compat_patches
+
+        apply_admin_security()
+        apply_compat_patches()
